695c9f7269f21377baa3f75469cc237c09d0c25e
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-01-02 19:06:30 +0600

    validation check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,33 @@
 (function ($) {
 
 
-  // HERO SLIDER
-//   var menu = [];
-//   jQuery('.swiper-slide').each(function (index) {
-//       menu.push(jQuery(this).find('.slid-inner').attr("data-text"));
-//   });
-//   var interleaveOffset = 0.5;
-//   var swiperOptions = {
-//       loop: true,
-//       speed: 1000,
-//       parallax: true,
-//       autoplay: {
-//           delay: 6500,
-//           disableOnInteraction: false,
-//       },
-//       watchSlidesProgress: true,
-//       pagination: {
-//           el: '.swiper-pagination',
-//           clickable: true,
-//       },
 
-//       navigation: {
-//           nextEl: '.swiper-button-next',
-//           prevEl: '.swiper-button-prev',
-//       },
 
-//       on: {
-//           progress: function () {
-//               var swiper = this;
-//               for (var i = 0; i < swiper.slides.length; i++) {
-//                   var slideProgress = swiper.slides[i].progress;
-//                   var innerOffset = swiper.width * interleaveOffset;
-//                   var innerTranslate = slideProgress * innerOffset;
-//                   swiper.slides[i].querySelector(".slid-inner").style.transform =
-//                       "translate3d(" + innerTranslate + "px, 0, 0)";
-//               }
-//           },
 
-//           touchStart: function () {
-//               var swiper = this;
-//               for (var i = 0; i < swiper.slides.length; i++) {
-//                   swiper.slides[i].style.transition = "";
-//               }
-//           },
 
-//           setTransition: function (speed) {
-//               var swiper = this;
-//               for (var i = 0; i < swiper.slides.length; i++) {
-//                   swiper.slides[i].style.transition = speed + "ms";
-//                   swiper.slides[i].querySelector(".slid-inner").style.transition =
-//                       speed + "ms";
-//               }
-//           }
-//       }
-//   };
 
-//   var swiper = new Swiper(".swiper-container", swiperOptions);
+    
+    //Setting hero slider
+    function heroSlider() {
+        if ($(".swiper-wrapper").length) {
+            $(".swiper-wrapper").slick({
+                arrows: false,
+                prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+                nextArrow: '<button type="button" class="slick-next">Next</button>',
+                dots: true,
+                fade: true,
+                cssEase: 'linear',
+            });
+        }
+    }
+
+    //Active heor slider
+    heroSlider();
+
+
+
+
+
 
   // DATA BACKGROUND IMAGE
   var sliderBgSetting = $(".slide-bg-image");
@@ -152,7 +123,7 @@ $(".mobile-btn").click(function(){
         $(".service-s2-active").owlCarousel({
           autoplay: false,
           smartSpeed: 300,
-          margin: 20,
+          margin: 0,
           loop:true,
           autoplayHoverPause:true,
           dots: false,
@@ -423,7 +394,7 @@ if ($(".odometer").length) {
           autoplay: false,
           smartSpeed: 300,
           center: true,
-          margin: 30,
+          margin: 0,
           loop:true,
           autoplayHoverPause:true,
           dots: false,
